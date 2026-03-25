6cc833aa5e317687e160c81c4c2d5aed06bca6a4
filete <fileteminyon@gmail.com>
2026-03-25 01:36:58 +0100

    CLI11 implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
-#include "main.h"
+#include "main.hpp"
+#include "CLI11.hpp"
 
-int main(void) {
-  uint16_t cutoff{};
+int main(int argc, char* argv[]) {
+    CLI::App app{"WAV file processor"};
+    argv = app.ensure_utf8(argv);
 
-  std::cout << "Enter cutoff frequency: ";
-  std::cin >> cutoff;
+    std::string inputFile, outputFile;
+    uint16_t cutoff{};
 
-  WavFile testWav("guitar16b.wav");
+    app.add_option("input", inputFile, "Input WAV file name") -> required();
+    app.add_option("output", outputFile, "Output WAV file name") -> required();
+    app.add_option("-c,--cutoff",cutoff, "Filter cutoff frequency in Hz");
+
+    CLI11_PARSE(app, argc, argv);
+
+  WavFile testWav(argv[1]);
   testWav.applyFilter(cutoff);
-  testWav.write("output");
+  testWav.write(argv[2]);
 
   return 0;
 }
