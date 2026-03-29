c97073a4bd8088f46a5eda15ca6995a5b0138074
filete <fileteminyon@gmail.com>
2026-03-29 22:41:53 +0200

    CLI argumrnts for the synth functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,63 @@
 #include "main.hpp"
+#include "CLI11.hpp"
 
-int main(int argc, char* argv[]) {
-    CLI::App app{"WAV file processor"};
-    argv = app.ensure_utf8(argv);
+int main(int argc, char *argv[]) {
+  CLI::App app{"WAV file processor"};
+  argv = app.ensure_utf8(argv);
 
-    std::string inputFile, outputFile, fileDirectory;
-    uint16_t cutoff{};
+  std::string inputFile, outputFile, fileDirectory, mode;
+  uint16_t cutoff{}, waveTypeI{0};
+  float frequency{440.0f}, duration{1.0f}, amp{0.85};
+  bool flag_headerInfo{false};
 
-    app.add_option("input", inputFile, "Input WAV file name") -> required();
-    app.add_option("output", outputFile, "Output WAV file name") -> required();
-    app.add_option("-c,--cutoff",cutoff, "Filter cutoff frequency in Hz");
-    app.add_option("-d,--file-dir", fileDirectory, "Input file directory");
+  app.add_option("-m,--mode", mode, "Program mode(filter | synth)")->required();
+  app.add_option("-i,--input-file", inputFile, "Input WAV file name");
+  app.add_option("-o,--output", outputFile, "Output WAV file name")->required();
+  app.add_option("-c,--cutoff", cutoff, "Filter cutoff frequency in Hz")
+      ->check(CLI::Range(0, 20000));
+  app.add_option("-w,--wave", waveTypeI,
+                 "Wave type to sinthesize (0 [sine] | 1 [square] | 2 "
+                 "[triangle] | 3 [sawtooth])")
+      ->check(CLI::Range(0, 3));
+  app.add_option("-f,--frequency", frequency, "Wave frequency to synth in Hz")
+      ->check(CLI::Range(0.0, 20000.0));
+  app.add_option("-d,--duration", duration,
+                 "Duration of the synthed output file")
+      ->check(CLI::Range(0.2, 30.0));
+  app.add_option("-a,--amplitude", amp, "Amplitude of the synthed output file")
+      ->check(CLI::Range(0.0, 1.0));
+  app.add_option("--file-dir", fileDirectory, "Input file directory");
+  app.add_flag("--header-info", flag_headerInfo, "Flag to print file info.");
 
-    CLI11_PARSE(app, argc, argv);
+  CLI11_PARSE(app, argc, argv);
 
-
-  WavFile testWav(inputFile);
-  if(!fileDirectory.empty()){
+  if (mode == "filter") {
+    WavFile testWav(inputFile);
+    if (!fileDirectory.empty()) {
       testWav.setFilePath(fileDirectory);
       std::cout << "File path set to: " << testWav.getFilePath() << std::endl;
-  }
-  if(cutoff){
-      std::cout << "Applying filter with cutoff frequency: " << cutoff << " Hz" << std::endl;
+    }
+    if (cutoff) {
+      std::cout << "Applying filter with cutoff frequency: " << cutoff << " Hz"
+                << std::endl;
       testWav.applyFilter(cutoff);
-  }else{
+    } else {
       testWav.applyFilter();
-  }
-  testWav.write(outputFile);
+    }
+    testWav.write(outputFile);
 
+    if (flag_headerInfo)
+      testWav.getHeader().printState();
+  } else {
+    WavFile synthWav(outputFile, 16, 1, 44100);
+    if (!fileDirectory.empty()) {
+      synthWav.setFilePath(fileDirectory);
+      std::cout << "File path set to: " << synthWav.getFilePath() << std::endl;
+    }
+    synthWav.synthAudio(static_cast<SynthWave::WaveType>(waveTypeI), duration,
+                        frequency, amp);
+    if (flag_headerInfo)
+      synthWav.getHeader().printState();
+  }
   return 0;
 }
